97ac46d62e08d24bd91dea4ef9d1771214b98633
Erivania Leal <erivanial.s1507@gmail.com>
2026-07-18 19:48:49 -0300

    inicia o projeto

---
+++ src/main/java/Esse/projeto/Mantem/as/entidades/Cliente/e/Pet/ClientePetApplication.java
@@ -0,0 +1,13 @@
+package Esse.projeto.Mantem.as.entidades.Cliente.e.Pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientePetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientePetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Esse/projeto/Mantem/as/entidades/Cliente/e/Pet/ClientePetApplicationTests.java
@@ -0,0 +1,13 @@
+package Esse.projeto.Mantem.as.entidades.Cliente.e.Pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
