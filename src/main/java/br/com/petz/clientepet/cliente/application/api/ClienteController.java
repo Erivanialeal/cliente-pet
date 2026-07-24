ea0402d2ec8d800bd20843682186e488e50bd2f5
Erivania Leal <erivanial.s1507@gmail.com>
2026-07-24 20:05:56 -0300

    implementa controller e fluxo para service

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -1,16 +1,22 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import br.com.petz.clientepet.cliente.application.service.ClienteService;
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
 public class ClienteController implements ClienteApi {
+    private final ClienteService clienteService;
+
     @Override
     public ClienteResponse postCliente(ClienteResquest clienteResquest) {
         log.info("[inicia]ClienteController - postCliente");
+        ClienteResponse clienteCriado = clienteService.criaCliente(clienteResquest);
         log.info("[finaliza]ClienteController - postCliente");
-        return null;
+        return clienteCriado;
     }
 }
