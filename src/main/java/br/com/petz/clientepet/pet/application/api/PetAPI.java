cb6496ea6d8a616f765cde232fc432044e839648
Erivania Leal <erivanial.s1507@gmail.com>
2026-08-07 20:17:22 -0300

    implementa conversão

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetAPI.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetAPI.java
@@ -1,5 +1,6 @@
 package br.com.petz.clientepet.pet.application.api;
 
+import br.com.petz.clientepet.cliente.application.api.ClienteDetalhadoResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,11 @@ public interface PetAPI {
     @GetMapping
     @ResponseStatus(code = HttpStatus.OK)
     List<PetClienteListResponse> getPetsDoClienteComId(@PathVariable UUID idCliente);
+
+    @GetMapping(value = "/{idPet}")
+    @ResponseStatus(code = HttpStatus.OK)
+    ClienteDetalhadoResponse getTodosClienteAtravesId(@PathVariable UUID idCliente, @PathVariable UUID idPet);
+
 
 
 }
