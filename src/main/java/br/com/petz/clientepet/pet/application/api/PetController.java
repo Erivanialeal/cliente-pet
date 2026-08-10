7c0c1c12b581e1b8732788189096d3ac98b12f91
Erivania Leal <erivanial.s1507@gmail.com>
2026-08-09 21:22:44 -0300

    pet patch corrige retorno para void

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
@@ -52,10 +52,10 @@ public class PetController implements PetAPI {
     }
 
     @Override
-    public PetResponse patchPet(UUID idCliente, UUID idPet, PetAlteracaoResquest petAlteracaoResquest) {
+    public void patchPet(UUID idCliente, UUID idPet, PetAlteracaoResquest petAlteracaoResquest) {
         log.info("[inicia] PetController -  patchPet");
         log.info("[idCliente] {} - [idPet] {}", idCliente, idPet);
+        
         log.info("[finaliza] PetController -  patchPet");
-        return null;
     }
 }
